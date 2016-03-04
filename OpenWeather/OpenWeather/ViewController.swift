ccadaa2d123a18acd3fa59fcc4f1236668032035
Dolecki, Eric <edolecki@gmail.com>
2016-03-04 14:02:25 -0500

    Adding bulk code.

--- OpenWeather/OpenWeather/ViewController.swift
+++ OpenWeather/OpenWeather/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var weatherFetch: WeatherFetch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        weatherFetch = WeatherFetch(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 50))
+        weatherFetch.updateTheWeather()
+        weatherFetch.beginFetchingWeather()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
--- OpenWeather/OpenWeather/WeatherFetch.swift
+++ OpenWeather/OpenWeather/WeatherFetch.swift
@@ -12,6 +12,9 @@ import UIKit
 class WeatherFetch: UIView {
     
     var myFrame: CGRect!
+    var APIKey = "60999e242afc6229cd8aabe41caeeea5"
+    var weatherFetchTimer: NSTimer!
+    var cityName = "boston"
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -19,5 +22,75 @@ class WeatherFetch: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        myFrame = frame
+    }
+    
+    func beginFetchingWeather(){
+        if weatherFetchTimer != nil {
+            weatherFetchTimer.invalidate()
+        }
+        
+        //We'll check the weather twice an hour.
+        
+        weatherFetchTimer = NSTimer.scheduledTimerWithTimeInterval(1800, target: self,
+            selector: "updateTheWeather", userInfo: nil, repeats: true)
+    }
+    
+    func stopFetchingWeather(){
+        if weatherFetchTimer != nil {
+            weatherFetchTimer.invalidate()
+        }
+    }
+    
+    func updateTheWeather(){
+        let baseUrl: String = "http://api.openweathermap.org/data/2.5/weather"
+        let url: String = "\(baseUrl)?q=\(cityName)&units=imperial&appid=\(APIKey)"
+        let finalUrl: NSURL = NSURL(string: url)!
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(finalUrl, completionHandler: {data, response, error -> Void in
+            if error != nil{
+                print(error!.localizedDescription)
+            }
+            
+            let qualityOfServiceClass = QOS_CLASS_BACKGROUND
+            let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
+            dispatch_async(backgroundQueue, {
+                
+                var err: NSError?
+                let reachability:Reachability
+                do {
+                    reachability = try Reachability.reachabilityForInternetConnection()
+                    if reachability.isReachable() == false {
+                        return
+                    }
+                } catch {
+                    print("can't create reachability.")
+                }
+                
+                let json = JSON(data: data!, options: NSJSONReadingOptions(), error: &err)
+                let weatherCurrent = json["main"]["temp"].stringValue
+                let wc = weatherCurrent as NSString
+                let floatwc = floor(wc.floatValue)
+                let intwc = Int(floatwc)
+                
+                let weatherHigh = json["main"]["temp_max"].stringValue
+                let wh = weatherHigh as NSString
+                let floatwh = floor(wh.floatValue)
+                let intwh = Int(floatwh)
+                
+                let weatherLow = json["main"]["temp_min"].stringValue
+                let wl = weatherLow as NSString
+                let floatwl = floor(wl.floatValue)
+                let intwl = Int(floatwl)
+                let weatherDescription = json["weather"][0]["description"].stringValue
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    //let icon = json["weather"][0]["icon"].stringValue
+                    let weatherString = "C: \(intwc)°F, H: \(intwh)°F, L: \(intwl)°F, cond: \(weatherDescription.capitalizedString)"
+                    print(weatherString)
+                })
+            })
+        })
+        task.resume()
     }
 }
