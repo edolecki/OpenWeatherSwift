d9508554cd3156f1c1fe4ccf775b97ef965be993
Dolecki, Eric <edolecki@gmail.com>
2016-03-04 13:44:00 -0500

    Initial project commit

---
+++ OpenWeather/OpenWeather/WeatherFetch.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherFetch.swift
+//  OpenWeather
+//
+//  Created by Eric Dolecki on 3/4/16.
+//  Copyright © 2016 Eric Dolecki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherFetch: UIView {
+    
+    var myFrame: CGRect!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
